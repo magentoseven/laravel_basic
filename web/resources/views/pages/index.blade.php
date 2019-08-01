a864e87dfb6795267a9b614d109d9b8ab04ab838
rendbacklaysh <hai.stuw@gmail.com>
2019-08-01 10:32:30 +0700

    Basic routing.

---
+++ web/resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel App')}}</title>
+  </head>
+  <body>
+    <h1>Welcome To Laravel</h1>
+    <p>This is the laravel application from the "Laravel From Scratch Youtube series.</p>
+  </body>
+</html>
